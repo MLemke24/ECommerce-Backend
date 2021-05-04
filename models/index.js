414b47f103be4ca748e30c135628f31f7c559544
mlemke24 <mattrlemke@gmail.com>
2021-05-03 20:34:40 -0500

    category api done

--- models/index.js
+++ models/index.js
@@ -7,24 +7,24 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
-})
+});
 
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id'
-})
+});
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey: 'category_id'
-})
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
   foreign_key: 'tag_id'
-})
+});
 
 module.exports = {
   Product,
